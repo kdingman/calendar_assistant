20b769d6f8b6502163a46a7373fa82d7bcc3e07c
Kelly Dingman <kelly@Kellys-MacBook-Pro.local>
2022-01-02 12:50:02 -0500

    pull from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,19 @@ $(".saveBtn").on("click", function() {
 // local storage
     localStorage.setItem(taskInfo, time);
 });
+
+// add saved data from local storage
+$("#7 .description").val(localStorage.getItem("7"));
+$("#8" .description).val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
