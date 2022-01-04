c7e01cd979f71628622e5a16f4ac5c14b0e641cd
Kelly Dingman <kelly@Kellys-MacBook-Pro.local>
2022-01-04 16:17:43 -0500

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,26 +18,6 @@ var tasks = {
     "18": [],
     "19": [],
 };
-
-// Push and Pull Local Storage
-var pushTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-var pullTasks = function() {
-    var enteredTasks = JSON.parse(localStorage.getItem("tasks"));
-        if(enteredTasks) {
-            tasks = enteredTasks
-
-            $.each(tasks, function(hour, task) {
-                var hourTask = $("#" + hour);
-                createTask(task, hourTask);
-            })
-        }
-    // Double check all tasks are pulled and pushed
-        checkTasks()
-}
-
 // Work with Tasks
 var createTask = function(taskText, hourTask) {
     var taskTasks = hourTask.find(".task");
