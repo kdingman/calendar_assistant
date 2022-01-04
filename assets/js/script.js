9555f189a9f06f619812ee27d66cc26e11f93e8a
Kelly Dingman <kelly@Kellys-MacBook-Pro.local>
2022-01-04 15:47:21 -0500

    updated local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,4 +30,43 @@ var tasks = {
     "19": [],
 };
 
+// Work with Tasks
+var createTask = function(taskText, hourTask) {
+    var taskTasks = hourTask.find(".task");
+    var taskPlace = $("<p>")
+        .addClass("description")
+        .text(taskText)
+    hourTask.html(taskPlace);
+}
 
+var checkTasks = function() {
+
+    var currentHour = moment().hour();
+        $(".task-info").each(function () {
+            var taskHour = parseInt($(this).attr("id"));
+
+        // past, present, future tasks
+        if(taskHour < currentHour) {
+            $(this).removeClass(["present", "future"]).addClass("past");
+        }
+        else if(taskHour === currentHour) {
+            $(this).removeClass(["past", "future"]).addClass("present");
+        }
+        else {
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+    })
+};
+
+var returnTextArea = function(textareaElement) {
+    var taskInfo = textareaElement.closest(".task-info");
+    var textArea = taskInfo.find("textarea");
+
+    var hour = taskInfo.attr("id");
+    var text = textArea.val().trim();
+
+    tasks [hour] = [text];
+    pushTasks();
+
+    createTask(text, taskInfo);
+}
