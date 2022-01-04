8dabbcb94b07541042c0e99b42c3be3aab5c8fe2
Kellly <kcdonlan@yahoo.com>
2022-01-04 17:12:34 -0500

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,33 @@
 var today = moment();
 $("#currentDay").text(today.format('dddd MMMM Do, YYYY'));
 
+// save button clicks
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        //values
+        var task = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
+
+        localStorage.setItem(task, hour);
+    })
+    // load saved information from local storage
+    $("#7 .description").val(localStorage.getItem("7"));
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .descrption").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
+    $("#19 .description").val(localStorage.getItem("19"));
+});
+
 // Create Hour Task Objects
-var tasks = {
+var taskTimes = {
     "7": [],
     "8": [],
     "9": [],
@@ -19,62 +44,36 @@ var tasks = {
     "19": [],
 };
 
-// Push and Pull Local Storage
-var pushTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+function timeTracker() {
 
-var pullTasks = function() {
-    var enteredTasks = JSON.parse(localStorage.getItem("tasks"));
-        if(enteredTasks) {
-            tasks = enteredTasks
+    // check on current time
+    var currentTime = moment().hour();
 
-            $.each(tasks, function(hour, task) {
-                var hourTask = $("#" + hour);
-                createTask(task, hourTask);
-            })
+    // loop through time blocks
+    $(".time-block").each(function() {
+        var taskTimes = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(taskTimes, currentTime)
+
+        // past, present, future via HTML and CSS
+        if(taskTimes < currentTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
         }
-    // Double check all tasks are pulled and pushed
-        checkTasks()
-}
-
-// Work with Tasks
-var createTask = function(taskText, hourTask) {
-    var taskTasks = hourTask.find(".task");
-    var taskPlace = $("<p>")
-        .addClass("description")
-        .text(taskText)
-    hourTask.html(taskPlace);
-}
-
-var checkTasks = function() {
-
-    var currentHour = moment().hour();
-        $(".task-info").each(function () {
-            var taskHour = parseInt($(this).attr("id"));
-
-        // past, present, future tasks
-        if(taskHour < currentHour) {
-            $(this).removeClass(["present", "future"]).addClass("past");
-        }
-        else if(taskHour === currentHour) {
-            $(this).removeClass(["past", "future"]).addClass("present");
+        else if(taskTimes === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
         }
         else {
-            $(this).removeClass(["past", "present"]).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
         }
     })
-};
 
-var returnTextArea = function(textareaElement) {
-    var taskInfo = textareaElement.closest(".task-info");
-    var textArea = taskInfo.find("textarea");
+    }
+        timeTracker();
 
-    var hour = taskInfo.attr("id");
-    var text = textArea.val().trim();
-
-    tasks [hour] = [text];
-    pushTasks();
-
-    createTask(text, taskInfo);
-}
+    // check current time
+    var duration = setInterval(timeTracker, 15000);
