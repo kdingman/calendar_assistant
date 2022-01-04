0694c27078b0655cf8987b6872d428640b394615
Kelly Dingman <kelly@Kellys-MacBook-Pro.local>
2022-01-04 15:40:20 -0500

    fixed local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+// save button clicks
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        //values
+        var task = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
+
+        localStorage.setItem(task, hour);
+    });
+})
+
 // Create Hour Task Objects
 var tasks = {
     "7": [],
@@ -19,21 +30,4 @@ var tasks = {
     "19": [],
 };
 
-// Push and Pull Local Storage
-var pushTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
 
-var pullTasks = function() {
-    var enteredTasks = JSON.parse(localStorage.getItem("tasks"));
-        if(enteredTasks) {
-            tasks = enteredTasks
-
-            $.each(tasks, function(hour, task) {
-                var hourTask = $("#" + hour);
-                createTask(task, hourTask);
-            })
-        }
-    // Double check all tasks are pulled and pushed
-        checkTasks()
-}
