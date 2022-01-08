d7a4de9f0fe675fdec4c5fbe3b12ff5a7fceabf7
Kellly <kcdonlan@yahoo.com>
2022-01-08 10:38:07 -0500

    updated time function and added military time array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
-// add date to top of screen
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+// add date and time to top of screen
+var currentHour = moment().format('LLL');
+var militaryTime = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+var saveBtn = document.querySelector(".saveBtn");
+    console.log(currentHour);
 
-// Create Hour Task Objects
-var tasks = {
-    "7": [],
-    "8": [],
-    "9": [],
-    "10": [],
-    "11": [],
-    "12": [],
-    "13": [],
-    "14": [],
-    "15": [],
-    "16": [],
-    "17": [],
-    "18": [],
-    "19": [],
-};
