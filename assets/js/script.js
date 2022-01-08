95ca41073252959ba534bd697a52b2483232b40b
Kellly <kcdonlan@yahoo.com>
2022-01-08 11:12:58 -0500

    time updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // add date and time to top of screen
-var currentHour = moment().format('LLL');
+var currentTime = moment().hour();
+// Set Military Time Array
 var militaryTime = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+// click button
 var saveBtn = document.querySelector(".saveBtn");
     console.log(currentHour);
-
