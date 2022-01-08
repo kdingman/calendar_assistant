73edfe909b191fd7dc93edef12394ea141262dea
Kellly <kcdonlan@yahoo.com>
2022-01-08 11:27:15 -0500

    fixed wording

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,4 @@ var currentTime = moment().hour();
 var militaryTime = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 // click button
 var saveBtn = document.querySelector(".saveBtn");
-    console.log(currentHour);
+    console.log(currentTime);
