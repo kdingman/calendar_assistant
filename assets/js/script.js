cbc97951675b2a71c456424a55ce9a4aaa2432bd
Kellly <kcdonlan@yahoo.com>
2022-01-09 10:55:54 -0500

    finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,32 @@
     $("#currentDay").text(moment().format('LLLL'));
 
     var saveBtn = $(".saveBtn");
+
+        // Button Click
+        saveBtn.on("click", function() {
+
+            var hour = $(this).siblings(".hour").text();
+            var userText = $(this).siblings(".description").val();
+    
+    // save to local storage
+        JSON.parse(window.localStorage.setItem(hour, userText));
+        JSON.parse(window.localStorage.getItem(hour, userText));
+    });
+
+        // upload any saved data from local storage
+        $("#7 .description").val(localStorage.getItem("7:00AM"));
+        $("#8 .description").val(localStorage.getItem("8:00AM"));
+        $("#9 .description").val(localStorage.getItem("900AM"));
+        $("#10 .description").val(localStorage.getItem("10:0AM"));
+        $("#11 .description").val(localStorage.getItem("11:0AM"));
+        $("#12 .description").val(localStorage.getItem("12:00PM"));
+        $("#13 .description").val(localStorage.getItem("1:00PM"));
+        $("#14 .description").val(localStorage.getItem("2:00PM"));
+        $("#15 .description").val(localStorage.getItem("3:00PM"));
+        $("#16 .description").val(localStorage.getItem("4:00PM"));
+        $("#17 .description").val(localStorage.getItem("5:00PM"));
+        $("#18 .description").val(localStorage.getItem("6:00PM"));
+        $("#19 .description").val(localStorage.getItem("7:00PM"));
         
     // Manage Tasks, past, present, future
     function manageTasks() {
@@ -20,34 +46,9 @@
                 $(this).addClass("past");
             }
         })
-    };
-
-    // Button Click
-    saveBtn.on("click", function() {
-
-            var hour = $(this).siblings(".hour").text();
-            var userText = $(this).siblings(".description").val();
-    
-    // save to local storage
-        localStorage.setItem(hour, userText);
-    });
-
-        // upload any saved data from local storage
-        $("#7 .description").val(localStorage.getItem("7:00AM"));
-        $("#8 .description").val(localStorage.getItem("8:00AM"));
-        $("#9 .description").val(localStorage.getItem("900AM"));
-        $("#10 .description").val(localStorage.getItem("10:0AM"));
-        $("#11 .description").val(localStorage.getItem("11:0AM"));
-        $("#12 .description").val(localStorage.getItem("12:00PM"));
-        $("#13 .description").val(localStorage.getItem("1:00PM"));
-        $("#14 .description").val(localStorage.getItem("2:00PM"));
-        $("#15 .description").val(localStorage.getItem("3:00PM"));
-        $("#16 .description").val(localStorage.getItem("4:00PM"));
-        $("#17 .description").val(localStorage.getItem("5:00PM"));
-        $("#18 .description").val(localStorage.getItem("6:00PM"));
-        $("#19 .description").val(localStorage.getItem("7:00PM"));
-    
-  
+    };  
     manageTasks();
 
     var interval = setInterval(manageTasks, 19000);
+
+    localStorage.clear();
